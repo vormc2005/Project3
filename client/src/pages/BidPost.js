ad6d4aab819760f0eb5ead62358eb157a04303ad
aya <aya.yoshida8@gmail.com>
2020-03-05 14:14:31 -0500

    client folder

---
+++ client/src/pages/BidPost.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Bid extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+}
+
+export default Bid;
