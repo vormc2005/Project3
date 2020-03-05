e871a243ffe16d7beffd2eade456fed06225d3d8
Anna Panas <annapanas0906@gmail.com>
2020-03-05 15:38:14 -0500

    working on Mongoose connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
-// app.use(bodyParser.json());
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`)
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/auction");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
