c4ca39d2ca660b93de113884784c7cc949358d7a
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-03-06 14:41:58 -0500

    displaying data from db as cards

--- client/src/pages/BidPost.js
+++ client/src/pages/BidPost.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import API from '../utils/API';
+import React, { Component } from "react";
+import API from "../utils/API";
 
 class Bid extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      result: [],
+      results: [],
       search: "",
       items: []
-    }
+    };
   }
 
   componentDidMount() {
@@ -16,20 +16,24 @@ class Bid extends Component {
   }
 
   displayData = query => {
-    API.getAllItems(query).then(res => {
-      this.setState({
-        results: res.data.results
+    API.getAllItems(query)
+      .then(res => {
+        this.setState({
+          results: res.data
+        });
       })
-    }).catch(err => console.log(err));
-  }
+      .catch(err => console.log(err));
+  };
 
   searchData = query => {
-    API.getItem(query).then(res => {
-      this.setState({
-        results: res.data.results
+    API.getItem(query)
+      .then(res => {
+        this.setState({
+          results: res.data
+        });
       })
-    }).catch(err => console.log(err));
-  }
+      .catch(err => console.log(err));
+  };
 
   handleInputChange = event => {
     const value = event.target.value;
@@ -37,15 +41,15 @@ class Bid extends Component {
     this.setState({
       [name]: value
     });
-  }
+  };
 
   handleUserSubmit = event => {
     event.preventDefault();
-    this.searchUser(this.state.search);
-  }
+    this.searchData(this.state.search);
+  };
 
   render() {
-    return(
+    return (
       <div className="container">
         <div className="jumbotron jumbotron-fluid">
           <div className="container">
@@ -53,33 +57,52 @@ class Bid extends Component {
             <p className="lead">Search through the items and place a bid!</p>
           </div>
         </div>
-        {/* <div className="btn-group">
-          <button type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false">
-            Choose a category to search
-          </button>
-          <div className="dropdown-menu dropdown-menu-left">
-            <button className="dropdown-item" type="button">Home and Garden</button>
-            <button className="dropdown-item" type="button">Electronics</button>
-            <button className="dropdown-item" type="button">Fashion</button>
-            <button className="dropdown-item" type="button">Sporting Goods</button>
-            <button className="dropdown-item" type="button">Business and Industrial</button>
-          </div>
-        </div> */}
         <div className="col-6">
           <select className="itemSearch">
-            <option id="allItems" value="allItems">All Items</option>
-            <option id="homeAndGarden" value="homeAndGarden">Home and Garden</option>
-            <option id="electronics" value="electronics">Electronics</option>
-            <option id="fashion" value="fashion">Fashion</option>
-            <option id="sportingGoods" value="sportingGoods">Sporting Goods</option>
-            <option id="businessIndustrial" value="businessIndustrial">Business and Industrial</option>
+            <option id="allItems" value="allItems">
+              All Items
+            </option>
+            <option
+              id="homeAndGarden"
+              name="homeAndGarden"
+              value="homeAndGarden"
+            >
+              Home and Garden
+            </option>
+            <option id="electronics" name="electronics" value="electronics">
+              Electronics
+            </option>
+            <option id="fashion" name="electronics" value="fashion">
+              Fashion
+            </option>
+            <option
+              id="sportingGoods"
+              name="sportingGoods"
+              value="sportingGoods"
+            >
+              Sporting Goods
+            </option>
+            <option
+              id="businessIndustrial"
+              name="businessIndustrial"
+              value="businessIndustrial"
+            >
+              Business and Industrial
+            </option>
           </select>
-          <button className="btn btn-outline-secondary" type="button" id="searchAlcBtn">Search</button>
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="searchAlcBtn"
+            // onChange={this.handleInputChange()}
+          >
+            Search
+          </button>
         </div>
-        <div className="card mb-3" style={{ width: "90%" }}>
-          {this.state.items.filter(item =>
-          (item.category).map(item => {
-            return(
+
+        {this.state.results.map(item => {
+          return (
+            <div className="card mb-3" style={{ width: "90%" }}>
               <div className="row no-gutters">
                 <div className="col-md-4">
                   <img src={item.image} className="card-img" alt="..." />
@@ -88,18 +111,18 @@ class Bid extends Component {
                   <div className="card-body">
                     <h5 className="card-title">{item.itemname}</h5>
                     <p className="card-text">{item.condition}</p>
-                    <p className="card-text"><small className="text-muted">{item.startingbid}</small></p>
+                    <p className="card-text">
+                      <small className="text-muted">{item.startingbid}</small>
+                    </p>
                   </div>
                 </div>
-              </div>)
-          }))
-          }
-        </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-
-    )
+    );
   }
-
 }
 
 export default Bid;
