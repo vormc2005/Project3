e0390d21c4585c7c7e4430059a75fdf13fa193cc
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-03-09 12:05:49 -0400

    changed drop down to input search and edited spelling in seeds

--- client/src/pages/BidPost.js
+++ client/src/pages/BidPost.js
@@ -47,7 +47,7 @@ class Bid extends Component {
 
   };
 
-  //Function that delets items by ID//
+  //Function that deletes items by ID//
 
   deleteItem = (id) => {
     API.deleteItem(id)
@@ -60,10 +60,13 @@ class Bid extends Component {
     const name = event.target.name;
     this.setState({
       [name]: value
-    }, () => {
-      console.log(this.state);
-      this.displayData(this.state.search)
-    });
+    }
+      //commented this out because I'm not sure what it does and it wasn't letting my search function work... we probs need it tho
+      // , () => {
+      //   console.log(this.state);
+      //   this.displayData(this.state.search)
+      // }
+    );
   };
 
   handleBidSubmit = event => {
@@ -71,8 +74,8 @@ class Bid extends Component {
     this.searchData(this.state.search);
   };
 
-  placeBid =()=>{
-  
+  placeBid = () => {
+
   }
 
   // handleBuyNow = event =>{
@@ -91,7 +94,17 @@ class Bid extends Component {
           </div>
         </div>
         <div className="col-6">
-          <select className="itemSearch" name="search" onChange={this.handleInputChange}>
+          <input
+            className="input"
+            placeholder="Search by Item Name or Category Here"
+            type="text"
+            onChange={this.handleInputChange.bind(this)}
+            name="search"
+            value={this.state.search}
+            style={{ width: 285 }}
+          ></input>
+          {/* Commented out dropdown, didn't want to delete */}
+          {/* <select className="itemSearch" name="search" onChange={this.handleInputChange}>
             <option id="allItems" value="" name="search"  >
               All Items
             </option>
@@ -123,7 +136,7 @@ class Bid extends Component {
             >
               Business and Industrial
             </option>
-          </select>
+          </select> */}
           {/* <button
             className="btn btn-outline-secondary"
             type="button"
@@ -135,48 +148,48 @@ class Bid extends Component {
           </button> */}
         </div>
 
-        {this.state.results.map(item => {
+        {/* filter allows us to search by item name or category, but only first word of category (awk) */}
+        {this.state.results.filter(item => (item.itemname).toLowerCase().trim().includes(this.state.search.toLowerCase().trim()) || (item.category).toLowerCase().includes(this.state.search.toLowerCase())).map(item => {
           return (
-            
-              <div className="card mb-3" style={{ width: "90%" }}>
-                <div className="row no-gutters">
-                  <div className="col-md-4">
-                    <img src={item.image} className="card-img" alt="..." />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">{item.itemname}</h5>
-                      <p className="card-text">{item.condition}</p>
-                      <div className="row">
-                        <div className="col-md-4">
-                          <p className="card-text">
-                            Highest Bid: $   
+            <div className="card mb-3" style={{ width: "90%" }}>
+              <div className="row no-gutters">
+                <div className="col-md-4">
+                  <img src={item.image} className="card-img" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.itemname}</h5>
+                    <p className="card-text">Condition: {item.condition}</p>
+                    <div className="row">
+                      <div className="col-md-4">
+                        <p className="card-text">
+                          Highest Bid: $
                       <font className="text-muted">{item.startingbid}</font>
-                          </p>
-                          {/*Here goes Bid Update price and logic to check if Bid equals to Buy now, if it does than it goes to purchase function*/}
+                        </p>
+                        {/*Here goes Bid Update price and logic to check if Bid equals to Buy now, if it does than it goes to purchase function*/}
 
-                          {/* <button className="btn btn-outline-secondary" type="text" onChange={this.handleBidSubmit}>Bid</button> */}
-                          <form>
-                            <label>
-                              <input type="text" name="name" style={{ width: "70%", borderRadius:"3px" }}/>
-                            </label>
-                            <input type="submit" value="Bid" className="btn btn-outline-secondary" />
-                          </form>
-                        </div>
-                        <div className="col-md-4">
-                          <p className="card-text">
-                            Buy Now: $
+                        {/* <button className="btn btn-outline-secondary" type="text" onChange={this.handleBidSubmit}>Bid</button> */}
+                        <form>
+                          <label>
+                            <input type="text" name="name" style={{ width: "70%", borderRadius: "3px" }} />
+                          </label>
+                          <input type="submit" value="Bid" className="btn btn-outline-secondary" />
+                        </form>
+                      </div>
+                      <div className="col-md-4">
+                        <p className="card-text">
+                          Buy Now: $
                       <font className="text-muted">{item.buyout}</font>
-                          </p>
-                          {/*Here goes delete function, alert tha notifies of successful purchase*/}
-                          <button className="btn btn-outline-secondary" onClick={() => this.deleteItem(item._id)}>Buy Now</button>
-                        </div>
+                        </p>
+                        {/*Here goes delete function, alert tha notifies of successful purchase*/}
+                        <button className="btn btn-outline-secondary" onClick={() => this.deleteItem(item._id)}>Buy Now</button>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            
+            </div>
+
           );
         })}
       </div>
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -14,20 +14,20 @@ function PostPage() {
     displayAll()
   }, [])
 
-  function displayAll()  {
+  function displayAll() {
     API.getAllItems()
       .then(res => setItems(res)
-        )      
+      )
       .catch(err => console.log(err));
   }
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
+    setFormObject({ ...formObject, [name]: value })
   };
 
   // When the form is submitted, use the API.saveItem method to save the item data
-  // Then reload itms from the database
+  // Then reload items from the database
   function handleSubmit(event) {
     event.preventDefault();
     if (formObject.itemname && formObject.startingbid) {
@@ -38,93 +38,93 @@ function PostPage() {
         condition: formObject.condition,
         buyout: formObject.condition
       })
-        .then(res =>  displayAll())
+        .then(res => displayAll())
         .catch(err => console.log(err));
     }
-  };  
-  
-    return (
-      <>
-       
-        <Navbar />
-        <br />
-        <h3>Fill out a form to upload your product!</h3>
-        <br></br>
-        <div className="container">
-          <div className="row">
-            <div className="col-4 offset-4">
-              <div className="card">
-                <form >
-                  <br></br>
-                  <div class="form-row">
-                    <div class="form-group col-md-12">
-                      <label htmlFor="itemName">Enter Item Name</label>
-                      <input id="itemName" name="itemName" type="text" class="form-control" onChange ={handleInputChange} />
-                    </div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-12">
-                      <label htmlFor="startPrice">Starting Price</label>
-                      <input id="startPrice" name="startPrice" type="startPrice" class="form-control" onChange ={handleInputChange} />
-                    </div>
-                    <div class="form-group col-md-12">
-                      <label htmlFor="startPrice">Buy Now</label>
-                      <input id="startPrice" name="startPrice" type="startPrice" class="form-control" onChange ={handleInputChange} />
-                    </div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-12">
-                      <label for="category">Choose a category</label>
-                      <select id="category" class="form-control" onChange ={handleInputChange}>
-                        <option>...</option>
-                        <option>Home and garden</option>
-                        <option>Electronics</option>
-                        <option>Fashion</option>
-                        <option>Sporting goods</option>
-                        <option>Business and industrial</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="form-row">
-                    <div class="form-group col-md-12">
-                      <label for="condition">Add item condition</label>
-                      <select id="condition" class="form-control" onChange ={handleInputChange}>
-                        <option>...</option>
-                        <option>New</option>
-                        <option>Good</option>
-                        <option>Used</option>
-                      </select>
-                    </div>
-                  </div>
-                  
-                  <div class="form-row">
-                    <div class="form-group col-md-12">
+  };
 
-                      <div class="file-upload">
-                        <div class="file-select">
+  return (
+    <>
 
-                          <label htmlFor="img"></label>
-                          <input id="img" name="img" type="file" />
+      <Navbar />
+      <br />
+      <h3>Fill out a form to upload your product!</h3>
+      <br></br>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 offset-4">
+            <div className="card">
+              <form >
+                <br></br>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label htmlFor="itemName">Enter Item Name</label>
+                    <input id="itemName" name="itemName" type="text" class="form-control" onChange={handleInputChange} />
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label htmlFor="startPrice">Starting Price</label>
+                    <input id="startPrice" name="startPrice" type="startPrice" class="form-control" onChange={handleInputChange} />
+                  </div>
+                  <div class="form-group col-md-12">
+                    <label htmlFor="startPrice">Buy Now</label>
+                    <input id="startPrice" name="startPrice" type="startPrice" class="form-control" onChange={handleInputChange} />
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label for="category">Choose a category</label>
+                    <select id="category" class="form-control" onChange={handleInputChange}>
+                      <option>...</option>
+                      <option>Home and garden</option>
+                      <option>Electronics</option>
+                      <option>Fashion</option>
+                      <option>Sporting goods</option>
+                      <option>Business and industrial</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+                    <label for="condition">Add item condition</label>
+                    <select id="condition" class="form-control" onChange={handleInputChange}>
+                      <option>...</option>
+                      <option>New</option>
+                      <option>Good</option>
+                      <option>Used</option>
+                    </select>
+                  </div>
+                </div>
 
-                        </div>
+                <div class="form-row">
+                  <div class="form-group col-md-12">
+
+                    <div class="file-upload">
+                      <div class="file-select">
+
+                        <label htmlFor="img"></label>
+                        <input id="img" name="img" type="file" />
+
                       </div>
-
                     </div>
-                  </div>
 
-                  <button className="postButton btn" onClick={handleSubmit}>Post Item!</button>
-                  <br />
-                </form>
-              </div>
-              <br></br>
+                  </div>
+                </div>
+
+                <button className="postButton btn" onClick={handleSubmit}>Post Item!</button>
+                <br />
+              </form>
             </div>
+            <br></br>
           </div>
         </div>
-        <Footer />
-      </>
+      </div>
+      <Footer />
+    </>
 
-    );
-  }
+  );
+}
 
 
 export default PostPage;
