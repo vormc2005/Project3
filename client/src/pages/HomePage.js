af2b4466c10d75e07076b50049384593c073ca24
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-03-10 23:03:09 -0400

    now comparing bid and buy prices!

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import BackgroundPic from "../components/BackgroundPic/index"
+//commented out since they're not being called...
+// import { Link } from "react-router-dom";
+// import BackgroundPic from "../components/BackgroundPic/index"
 import Footer from "../components/Footer/index"
-
-
 
 function Home() {
   return (
@@ -12,30 +11,21 @@ function Home() {
       <div class="container">
         <div class="row">
           <div class="col-12">
-
             <div class="card text-center">
               <div class="card-header home1">
-               <h1>Search, Post <span className="fun2">&</span> Bid </h1>
-              
-               <img src="/images/act.png" width="100px" height="100px" className="pic2" />
+                <h1>Search, Post <span className="fun2">&</span> Bid </h1>
+                <img alt="logo" src="/images/act.png" width="100px" height="100px" className="pic2" />
               </div>
-             
               <div class="card-body home">
-            
-              {/* <BackgroundPic backgroundImage="https://t4.ftcdn.net/jpg/00/99/17/81/240_F_99178133_4oUPBVYO1y7Py4M6otxiyfD9aSNLz3Vu.jpg" height="100px"/> */}
-              <img src="https://ofcc.ohio.gov/portals/0/Images/Resources/Bid%203D_Med.jpg" width="300px" height="150px" className="pic"/>
-              <br></br><br></br>
-              <h2>Explore our collections</h2>
-            <h3>Bid and Save</h3>
-           
-            
+                {/* <BackgroundPic backgroundImage="https://t4.ftcdn.net/jpg/00/99/17/81/240_F_99178133_4oUPBVYO1y7Py4M6otxiyfD9aSNLz3Vu.jpg" height="100px"/> */}
+                <img alt="logo" src="https://ofcc.ohio.gov/portals/0/Images/Resources/Bid%203D_Med.jpg" width="300px" height="150px" className="pic" />
+                <br></br><br></br>
+                <h2>Explore our collections</h2>
+                <h3>Bid and Save</h3>
               </div>
-             
               <div class="card-footer">
-            
               </div>
             </div>
-           
           </div>
         </div>
       </div>
