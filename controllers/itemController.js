56077049ba40c4067c5c40e796fd5266cbdb41c3
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-03-11 10:19:35 -0400

    updated bid price functional!

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -42,7 +42,7 @@ module.exports = {
   update: function (req, res) {
     console.log(req.params, req.body)
     db.Items
-      .findOneAndUpdate({ itemname: req.params.id }, req.body)
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
