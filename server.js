bdf4e85833964321d4a2af6fe1f411be5921390b
D V <voronov.dmitry@gmail.com>
2020-03-11 19:53:21 -0400

    gafsg

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(fileUpload())
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+ app.use(express.static(path.join(__dirname, './client/build')));
 }
 // Add routes, both API and view
 app.use(routes);
