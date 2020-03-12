a880f3cef047a54e8a8a145af87c623595546c40
D V <voronov.dmitry@gmail.com>
2020-03-12 09:39:34 -0400

    Alt tags are added

--- client/src/components/Social/social.js
+++ client/src/components/Social/social.js
@@ -9,12 +9,12 @@ import "./style.css";
 export default function SocialFollow() {
   return (
     <div>
-      <a href="https://www.twitter.com/bidbot4" className="twitter social" target="_blank">
+      <a href="https://www.twitter.com/bidbot4" className="twitter social" target="Twitter">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
       <a
         href="https://www.instagram.com/bid.b0t/"
-        className="instagram social" target="_blank">
+        className="instagram social" target="Instagram">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
     </div>
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -18,12 +18,12 @@ function Home() {
 
             <div class="card text-center">
               <div class="card-header home1">
-                  <img src="https://ofcc.ohio.gov/portals/0/Images/Resources/Bid%203D_Med.jpg" width="100px" height="100px" className="pic"/>
+                  <img src="https://ofcc.ohio.gov/portals/0/Images/Resources/Bid%203D_Med.jpg" width="100px" height="100px" alt="Bid_Med" className="pic"/>
                <h1 className="header"><Link to="./BidPost" className="link1">Search, </Link> <Link to="./PostPage"  className="link2">Post </Link><span className="fun2">& </span><Link to="./BidPost"  className="link3">Bid</Link> </h1>
             
 
               
-    <img src="/images/act.png" width="100px" height="100px" alt="act" className="pic2" />
+    <img src="/images/act.png" width="100px" height="100px" alt="act"  className="pic2" />
            
               </div>
              
@@ -39,32 +39,32 @@ function Home() {
 
 
 {/* process.env.PUBLIC_URL allows accessing  images from public folder */}
-  <img src={process.env.PUBLIC_URL + '/images/pic3.jpg'} height="200px" width="500px"/>
+  <img src={process.env.PUBLIC_URL + '/images/pic3.jpg'} height="200px" alt="pic3" width="500px"/>
   </Carousel.Item>
 
 
   <Carousel.Item>
    
-<img src={process.env.PUBLIC_URL + '/images/pic6.jpg'} height="200px" width="500px"/>
+<img src={process.env.PUBLIC_URL + '/images/pic6.jpg'} height="200px" alt="pic6" width="500px"/>
    
   </Carousel.Item>
 
   <Carousel.Item>
    
 
-<img src={process.env.PUBLIC_URL + '/images/pic8.jpg'} height="200px" width="500px"/>
+<img src={process.env.PUBLIC_URL + '/images/pic8.jpg'} height="200px" alt="pic8" width="500px"/>
   
 
   </Carousel.Item>
 
   <Carousel.Item>
   
-<img src={process.env.PUBLIC_URL + '/images/pic7.jpg'} height="200px" width="500px"/>
+<img src={process.env.PUBLIC_URL + '/images/pic7.jpg'} height="200px" alt="pic7" width="500px"/>
    
   </Carousel.Item>
   <Carousel.Item>
    
-<img src={process.env.PUBLIC_URL + '/images/pic5.jpg'} height="200px" width="500px"/>
+<img src={process.env.PUBLIC_URL + '/images/pic5.jpg'} height="200px" alt="pic5" width="500px"/>
    
 
   </Carousel.Item>
