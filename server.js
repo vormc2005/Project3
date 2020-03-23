878dbbd32a63f72d8fff0069b745581baf116d0c
D V <voronov.dmitry@gmail.com>
2020-03-23 18:10:32 -0400

    moved app.use

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use(fileUpload())
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
- app.use(express.static(path.join(__dirname, './client/build'))); 
-  
- 
-}
+  app.use(express.static(path.join(__dirname, './client/build')));
+ }
+
+ app.use(routes);
 // Add routes, both API and view
 
 
