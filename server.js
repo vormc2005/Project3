1a47d8d773545a64d631f02894fa5095a329c06f
D V <voronov.dmitry@gmail.com>
2020-03-23 18:13:32 -0400

    gsdf

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(fileUpload())
 //     app.get('/', (req, res) => {
 //         res.sendFile(path.join(__dirname, './client/build/'))
 //     });
-app.use(routes);
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, './client/build')));
